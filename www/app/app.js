5b62acf68284fba2e57b992f8a61a49a71a0bf23
Jan van Rossum <jan@vanrossumict.nl>
2015-11-08 20:12:09 +0000

    Added an API and a directive

--- www/app/app.js
+++ www/app/app.js
@@ -1,12 +1,67 @@
+'use strict';
+
 angular.module('sandbox', ['ngRoute'])
 
   .controller('MainController', function($scope) {
     $scope.stuff = 'lasdkfjlkasdjfklasf';
   })
 
-  .config(function($routeProvider) {
-    $routeProvider.when('/', {
-      template: '<h1>This is the homepage</h1>'
-    })
+  .controller('FirstController', function($scope, ApiService) {
+    //$scope.name = StringUtils.booyah('Darth Vader');
+    //$scope.name = booyah('Darth Vader');
+
+    ApiService.yesorno().then(function(data) {
+        $scope.yesorno = data;
+      }
+    );
   })
+
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/menu.html'
+      })
+      .when('/examples', {
+        templateUrl: 'app/examples/examples.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      })
+    ;
+  })
+
+  .service('ApiService', function ($http) {
+    this.yesorno = function() {
+      var payload = $http.get('http://yesno.wtf/api/');
+
+      payload.then(function (response) {
+        return response.data;
+      })
+      return $http.get('http://yesno.wtf/api/')
+      .then(function(response) {
+        return response.data;
+      });
+    }
+
+    this.imdbsearch = function() {
+      var payload = $http.get('http://www.omdbapi.com/?s=pulp+fiction');
+    }
+  })
+
+  .factory('booyah', function () {
+      return function (s) {
+         return s + "... BOOOOYAAAAAAAAH!!!";
+      };
+  })
+
+  .directive('add', function() {
+    return {
+      templateUrl: "app/directive.html",
+      scope: {
+        first: '=',
+        second: '='
+      }
+    }
+  })
+
 ;
